d4f7c0ff57c95b7cc6f52c6649eca1cd9fe390f6
winex01 <winnie131212592@gmail.com>
2023-11-11 22:01:33 +0800

    add my profile plugin to edit profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -7,7 +7,9 @@ use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Hasnayeen\Themes\ThemesPlugin;
 use Filament\Http\Middleware\Authenticate;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Routing\Middleware\SubstituteBindings;
@@ -37,10 +39,16 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
-            ->plugin(
-                // https://github.com/hasnayeen/themes
-                \Hasnayeen\Themes\ThemesPlugin::make()
-            )
+            ->plugins([
+                ThemesPlugin::make(), // https://github.com/hasnayeen/themes
+                BreezyCore::make() // https://github.com/jeffgreco13/filament-breezy
+                    ->myProfile(
+                        shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
+                        shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
+                        hasAvatars: false, // Enables the avatar upload form component (default = false)
+                        slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
+                    )
+            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -52,10 +60,9 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
 
-                // https://github.com/hasnayeen/themes
                 // Add Hasnayeen\Themes\Http\Middleware\SetTheme middleware to your provider middleware method or 
                 // if you're using filament multi-tenancy then instead add to tenantMiddleware method.
-                \Hasnayeen\Themes\Http\Middleware\SetTheme::class
+                \Hasnayeen\Themes\Http\Middleware\SetTheme::class // https://github.com/jeffgreco13/filament-breezy
             ])
             ->authMiddleware([
                 Authenticate::class,
