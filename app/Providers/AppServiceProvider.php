758181351b4769fee25f073081740312ff1dbe34
winex01 <winnie131212592@gmail.com>
2023-11-12 10:04:10 +0800

    add default model unguarded

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /** 
+         * Filament: Unguarding all models
+         * For brevity in this guide, we will disable Laravel's mass assignment protection. 
+         * Filament only saves valid data to models so the models can be unguarded safely. 
+         * To unguard all Laravel models at once, add Model::unguard() to the boot() method of 
+         * app/Providers/AppServiceProvider.php:
+         */ 
+        Model::unguard();
     }
 }
